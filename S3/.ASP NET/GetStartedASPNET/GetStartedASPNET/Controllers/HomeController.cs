2c5cc21a9911767ad9e1dfb74c8cb9e6fbed2c5f
Casper Meltesen <caspermeltesen@gmail.com>
2022-05-30 14:39:07 +0200

    ContosoUniversity update

---
+++ S3/.ASP NET/GetStartedASPNET/GetStartedASPNET/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GetStartedASPNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string H(string? mode, double? x, double? y)
+        {
+            Func<int, string> t = x => x.ToString();
+
+            if (mode == null || x == null || y == null) return "Brtuh";
+
+            #pragma warning disable CS8601 // Possible null reference assignment.
+            Dictionary<string, string> results = new Dictionary<string, string>
+            {
+                ["add"] = (x + y).ToString(),
+                ["sub"] = (x - y).ToString(),
+                ["mul"] = (x * y).ToString(),
+                ["div"] = (x / y).ToString(),
+                ["mod"] = (x % y).ToString(),
+            };
+            #pragma warning restore CS8601 // Possible null reference assignment.
+
+            return results[mode];
+        }
+    }
+}
---
+++ S3/.ASP NET/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        private decimal price;
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName="decimal(18,2)")]
+        public decimal Price { 
+            get { 
+                return price;
+            }
+            set { 
+                if (decimal.TryParse(value.ToString(), out decimal x)) price = x;
+            }
+        }
+    }
+}
